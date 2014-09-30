6fc2a91de58c3631101571a10258f7d3f675ecc9
mattn <mattn.jp@gmail.com>
2014-09-30 12:49:47 +0900

    Writer stdout in default

--- cmd/gosd/gosd.go
+++ cmd/gosd/gosd.go
@@ -15,24 +15,30 @@ func main() {
 		fmt.Println("Usage of " + os.Args[0] + ": gosd [filename]")
 	}
 	flag.Parse()
-	if flag.NArg() == 0 {
-		flag.Usage()
-		os.Exit(1)
-	}
+
 	var img image.Image
 	err := sixel.NewDecoder(os.Stdin).Decode(&img)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	f, err := os.Create(flag.Arg(0))
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	err = png.Encode(f, img)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+
+	if flag.NArg() == 0 {
+		err = png.Encode(os.Stdout, img)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	} else {
+		f, err := os.Create(flag.Arg(0))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		err = png.Encode(f, img)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 }
