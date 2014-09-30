b38b990eded4ef035e81225ef82fc22ea8bd7e87
mattn <mattn.jp@gmail.com>
2014-09-30 12:42:44 +0900

    Add Decoder

--- sixel.go
+++ sixel.go
@@ -1,9 +1,13 @@
 package sixel
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"image"
+	"image/color"
 	"io"
+	"strings"
 )
 
 type Encoder struct {
@@ -15,9 +19,9 @@ func NewEncoder(w io.Writer) *Encoder {
 }
 
 func (e *Encoder) Encode(img image.Image) error {
-	fmt.Fprintf(e.w, "\x1BP0;0;8q\"1;1")
+	fmt.Fprintf(e.w, "\x1bP0;0;8q\"1;1")
 	dx, dy := img.Bounds().Dx(), img.Bounds().Dy()
-	colors := map[uint32]int{}
+	colors := map[uint]int{}
 	nc := 0
 	for y := 0; y < dy; y++ {
 		for x := 0; x < dx; x++ {
@@ -25,7 +29,7 @@ func (e *Encoder) Encode(img image.Image) error {
 			r = r * 100 / 0xFFFF
 			g = g * 100 / 0xFFFF
 			b = b * 100 / 0xFFFF
-			v := r<<16 + g<<8 + b
+			v := uint(r<<16 + g<<8 + b)
 			if _, ok := colors[v]; !ok {
 				colors[v] = nc
 				fmt.Fprintf(e.w, "#%d;2;%d;%d;%d\n", nc, r, g, b)
@@ -39,7 +43,7 @@ func (e *Encoder) Encode(img image.Image) error {
 			r = r * 100 / 0xFFFF
 			g = g * 100 / 0xFFFF
 			b = b * 100 / 0xFFFF
-			v := r<<16 + g<<8 + b
+			v := uint(r<<16 + g<<8 + b)
 			idx := colors[v]
 			fmt.Fprintf(e.w, "#%d%c", idx, 63+1<<(uint(y)%6))
 		}
@@ -50,5 +54,133 @@ func (e *Encoder) Encode(img image.Image) error {
 		fmt.Fprint(e.w, "\n")
 	}
 	fmt.Fprint(e.w, "\x1B\\")
+	return nil
+}
+
+type Decoder struct {
+	r io.Reader
+	W int
+	H int
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{r, 200, 200}
+}
+
+func (e *Decoder) Decode(img *image.Image) error {
+	buf := bufio.NewReader(e.r)
+	c, err := buf.ReadByte()
+	if err != nil {
+		if err == io.EOF {
+			err = nil
+		}
+		return err
+	}
+	if c != '\x1B' {
+		return errors.New("Invalid format")
+	}
+	c, err = buf.ReadByte()
+	if err != nil {
+		return err
+	}
+	switch c {
+	case 'P':
+		s, err := buf.ReadString('q')
+		if err != nil {
+			return err
+		}
+		s = s[:len(s)-1]
+		tok := strings.Split(s, ";")
+		if len(tok) != 3 {
+			return errors.New("invalid format: illegal header tokens")
+		}
+	default:
+		return errors.New("Invalid format: illegal header")
+	}
+	c, err = buf.ReadByte()
+	if err != nil {
+		return err
+	}
+	if c == '"' {
+		s, err := buf.ReadString('#')
+		if err != nil {
+			return err
+		}
+		tok := strings.Split(s, ";")
+		if len(tok) != 2 {
+			return errors.New("invalid format: illegal size tokens")
+		}
+		err = buf.UnreadByte()
+		if err != nil {
+			return err
+		}
+	} else {
+		err = buf.UnreadByte()
+		if err != nil {
+			return err
+		}
+	}
+
+	colors := map[uint]color.Color{}
+	pimg := image.NewNRGBA(image.Rect(0, 0, e.W, e.H))
+	dx, dy := 0, 0
+data:
+	for {
+		c, err = buf.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return err
+		}
+		if c == '\r' || c == '\n' || c == '\b' {
+			continue
+		}
+		switch {
+		case c == '\x1b':
+			c, err = buf.ReadByte()
+			if err != nil {
+				return err
+			}
+			if c == '\\' {
+				break data
+			}
+		case c == '$':
+			dx = 0
+			dy++
+		case c == '-':
+		case c == '#':
+			err = buf.UnreadByte()
+			if err != nil {
+				return err
+			}
+			var nc, ci uint
+			var r, g, b uint
+			var c byte
+			n, err := fmt.Fscanf(buf, "#%d%c", &nc, &c)
+			if err != nil {
+				return err
+			}
+			if n != 2 {
+				return errors.New("invalid format: illegal data tokens")
+			}
+			if c == ';' {
+				n, err := fmt.Fscanf(buf, "%d;%d;%d;%d", &ci, &r, &g, &b)
+				if err != nil {
+					return err
+				}
+				if n != 4 {
+					return errors.New("invalid format: illegal data tokens")
+				}
+				colors[uint(nc)] = color.NRGBA{uint8(r*0xFF/100), uint8(g*0xFF/100), uint8(b*0xFF/100), 0XFF}
+			} else {
+				pimg.Set(dx, dy, colors[nc])
+				dx++
+			}
+		default:
+			return errors.New("invalid format: illegal data tokens")
+		}
+	}
+	*img = pimg
 	return nil
 }
