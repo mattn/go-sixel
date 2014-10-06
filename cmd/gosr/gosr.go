54d3f685846f997e580951ccdcd39fcd8d7a1183
mattn <mattn.jp@gmail.com>
2014-10-06 16:40:45 +0900

    Some image operation

--- cmd/gosr/gosr.go
+++ cmd/gosr/gosr.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"code.google.com/p/graphics-go/graphics"
 	"flag"
 	"fmt"
 	"image"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+	"math"
 	"os"
 
 	"github.com/mattn/go-sixel"
 )
+
+var fBlur = flag.String("blur", "", "Blur image by [Dev,Size]")
+var fResize = flag.String("resize", "", "Resize image by [WxH]")
+var fRotate = flag.Float64("rotate", 0.0, "Rotate image by [N] deg")
 
 func render(filename string) error {
 	var f *os.File
@@ -28,12 +34,56 @@ func render(filename string) error {
 	if err != nil {
 		return err
 	}
+	if *fResize != "" {
+		var w, h uint
+		fmt.Sscanf(*fResize, "%dx%d", &w, &h)
+		rx := float64(img.Bounds().Dx()) / float64(w)
+		ry := float64(img.Bounds().Dy()) / float64(h)
+		if rx < ry {
+			w = uint(float64(img.Bounds().Dx()) / ry)
+		} else {
+			h = uint(float64(img.Bounds().Dy()) / rx)
+		}
+		tmp := image.NewNRGBA64(image.Rect(0, 0, int(w), int(h)))
+		err = graphics.Scale(tmp, img)
+		if err != nil {
+			return err
+		}
+		img = tmp
+	}
+	if *fRotate != 0.0 {
+		d := math.Sqrt(math.Pow(float64(img.Bounds().Dx()), 2) + math.Pow(float64(img.Bounds().Dy()), 2))
+		sin, cos := math.Sincos(math.Atan2(float64(img.Bounds().Dx()), float64(img.Bounds().Dy())) + *fRotate)
+		if sin < cos {
+			sin = cos
+		} else {
+			cos = sin
+		}
+		tmp := image.NewNRGBA64(image.Rect(0, 0, int(cos * d), int(sin * d)))
+		err = graphics.Rotate(tmp, img, &graphics.RotateOptions{*fRotate})
+		if err != nil {
+			return err
+		}
+		img = tmp
+	}
+	if *fBlur != "" {
+		var d float64
+		var s int
+		fmt.Sscanf(*fBlur, "%f,%d", &d, &s)
+		tmp := image.NewNRGBA64(img.Bounds())
+		err = graphics.Blur(tmp, img, &graphics.BlurOptions{d, s})
+		if err != nil {
+			return err
+		}
+		img = tmp
+	}
 	return sixel.NewEncoder(os.Stdout).Encode(img)
 }
 
 func main() {
 	flag.Usage = func() {
 		fmt.Println("Usage of " + os.Args[0] + ": gosr [images]")
+		flag.PrintDefaults()
 	}
 	flag.Parse()
 	if flag.NArg() == 0 {
