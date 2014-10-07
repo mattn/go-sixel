3b0bdad41116d9b1d21f00e99a53bfd65e2ec132
mattn <mattn.jp@gmail.com>
2014-10-07 18:01:14 +0900

    Add example application

---
+++ _example/sixelview.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/mattn/go-gtk/gdkpixbuf"
+	"github.com/mattn/go-gtk/gtk"
+	"github.com/mattn/go-sixel"
+	"image"
+	"image/png"
+	"os"
+	"path/filepath"
+)
+
+type Image struct {
+	name string
+	img  image.Image
+}
+
+func main() {
+	var images []Image
+	if len(os.Args) == 1 {
+		var in image.Image
+		err := sixel.NewDecoder(os.Stdin).Decode(&in)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		images = append(images, Image{
+			name: "stdin",
+			img: in})
+
+	} else {
+		for _, arg := range os.Args[1:] {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			var in image.Image
+			err = sixel.NewDecoder(f).Decode(&in)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			f.Close()
+			images = append(images, Image{
+				name: filepath.Base(f.Name()),
+				img: in})
+		}
+	}
+
+	gtk.Init(nil)
+
+	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window.Connect("destroy", gtk.MainQuit)
+	notebook := gtk.NewNotebook()
+	for _, img := range images {
+		var buf bytes.Buffer
+		err := png.Encode(&buf, img.img)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		loader, gerr := gdkpixbuf.NewLoaderWithType("png")
+		if gerr != nil {
+			fmt.Fprintln(os.Stderr, gerr)
+			os.Exit(1)
+		}
+		_, gerr = loader.Write(buf.Bytes())
+		if gerr != nil {
+			fmt.Fprintln(os.Stderr, gerr)
+			os.Exit(1)
+		}
+
+		page := gtk.NewFrame(img.name)
+		notebook.AppendPage(page, gtk.NewLabel(img.name))
+		vbox := gtk.NewHBox(false, 1)
+		page.Add(vbox)
+
+		gimg := gtk.NewImage()
+		gimg.SetFromPixbuf(loader.GetPixbuf())
+		vbox.Add(gimg)
+	}
+	window.Add(notebook)
+	window.SetTitle("SixelViewer")
+	window.ShowAll()
+	gtk.Main()
+}
