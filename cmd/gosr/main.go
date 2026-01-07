d99d1249eaedb8471a45dec5d2c4a9d81cf553b3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-01-08 00:05:01 +0900

    support background color

--- cmd/gosr/main.go
+++ cmd/gosr/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"image"
+	"image/color"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
 	"math"
 	"os"
+	"runtime"
+	"strings"
+	"time"
 
 	"github.com/BurntSushi/graphics-go/graphics"
 	"github.com/mattn/go-sixel"
+	"golang.org/x/term"
 )
 
 var (
@@ -37,6 +41,27 @@ func render(filename string) error {
 	if err != nil {
 		return err
 	}
+
+	bounds := img.Bounds()
+	height := ((bounds.Dy()+5)/6)*6 + 7
+	tmp := image.NewNRGBA64(image.Rect(0, 0, bounds.Dx(), height))
+	for y := 0; y < bounds.Dy(); y++ {
+		for x := 0; x < bounds.Dx(); x++ {
+			r, g, b, a := img.At(bounds.Min.X+x, bounds.Min.Y+y).RGBA()
+			if a == 0 {
+				tmp.Set(x, y, bg)
+			} else {
+				tmp.Set(x, y, color.NRGBA64{uint16(r), uint16(g), uint16(b), 0xFFFF})
+			}
+		}
+	}
+	for y := bounds.Dy(); y < height; y++ {
+		for x := 0; x < bounds.Dx(); x++ {
+			tmp.Set(x, y, bg)
+		}
+	}
+	img = tmp
+
 	if *fResize != "" {
 		var w, h uint
 		fmt.Sscanf(*fResize, "%dx%d", &w, &h)
@@ -63,7 +88,7 @@ func render(filename string) error {
 			cos = sin
 		}
 		tmp := image.NewNRGBA64(image.Rect(0, 0, int(cos*d), int(sin*d)))
-		err = graphics.Rotate(tmp, img, &graphics.RotateOptions{*fRotate})
+		err = graphics.Rotate(tmp, img, &graphics.RotateOptions{Angle: *fRotate})
 		if err != nil {
 			return err
 		}
@@ -74,18 +99,63 @@ func render(filename string) error {
 		var s int
 		fmt.Sscanf(*fBlur, "%f,%d", &d, &s)
 		tmp := image.NewNRGBA64(img.Bounds())
-		err = graphics.Blur(tmp, img, &graphics.BlurOptions{d, s})
+		err = graphics.Blur(tmp, img, &graphics.BlurOptions{StdDev: d, Size: s})
 		if err != nil {
 			return err
 		}
 		img = tmp
 	}
-	buf := bufio.NewWriter(os.Stdout)
-	defer buf.Flush()
-
-	enc := sixel.NewEncoder(buf)
+	enc := sixel.NewEncoder(os.Stdout)
 	enc.Dither = true
 	return enc.Encode(img)
+}
+
+var bg = color.RGBA64{0, 0, 0, 0xFFFF}
+
+func init() {
+	if runtime.GOOS == "windows" {
+		return
+	}
+	tty, err := os.OpenFile("/dev/tty", os.O_RDWR, 0)
+	if err != nil {
+		return
+	}
+	defer tty.Close()
+
+	fd := int(tty.Fd())
+	oldState, err := term.MakeRaw(fd)
+	if err != nil {
+		return
+	}
+	defer term.Restore(fd, oldState)
+
+	tty.Write([]byte("\x1b]11;?\x1b\\"))
+	time.Sleep(100 * time.Millisecond)
+
+	var bb []byte
+
+	for {
+		var b [1]byte
+		_, err = tty.Read(b[:])
+		if err != nil || b[0] == '\\' || b[0] == 0x0a {
+			break
+		}
+		bb = append(bb, b[0])
+	}
+	if pos := strings.Index(string(bb), "rgb:"); pos != -1 {
+		bb = bb[pos+4:]
+		pos = strings.Index(string(bb), "\x1b")
+		if pos != -1 {
+			bb = bb[:pos]
+		}
+		var r, g, b uint16
+		n, err := fmt.Sscanf(string(bb), "%x/%x/%x", &r, &g, &b)
+		if err != nil || n != 3 {
+			bg = color.RGBA64{0, 0, 0, 0xFFFF}
+		} else {
+			bg = color.RGBA64{r, g, b, 0xFFFF}
+		}
+	}
 }
 
 func main() {
@@ -98,6 +168,7 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
+
 	for _, arg := range flag.Args() {
 		err := render(arg)
 		if err != nil {
